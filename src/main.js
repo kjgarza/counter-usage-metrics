df4579cfb09aaee076654c1cf464826017db1569
Kristian Garza <kj.garza@gmail.com>
2019-08-23 17:18:47 +0200

    removed font awesome

--- src/main.js
+++ src/main.js
@@ -2,13 +2,8 @@
 import Vue from 'vue/dist/vue.js'
 import App from './App.vue'
 import VueVega from 'vue-vega'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faUserSecret)
 
-Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.use(VueVega)
 
